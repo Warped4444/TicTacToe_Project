de7d30f14b141c959c18d9f0d7339673da42cace
Oscar J Soriano <ososoriano123@icloud.com>
2026-05-06 13:30:28 -0400

    Forgot to make a private void so I went back and fixed that

--- TicTacToe_1/MainWindow.xaml.cs
+++ TicTacToe_1/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace TicTacToe_1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
