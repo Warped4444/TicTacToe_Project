915b366794a8393e3ad596397ea06933a27a7892
Oscar J Soriano <ososoriano123@icloud.com>
2026-05-09 20:38:34 -0400

    I created 2 new classes one called Car and the other Button.

--- TicTacToe_1/MyClasses.cs
+++ TicTacToe_1/MyClasses.cs
@@ -6,5 +6,40 @@ using System.Threading.Tasks;
 
 namespace TicTacToe_1
 {
-    
+    class Car
+    {
+        String color = "red";
+        int currentSpeed = 0;
+        Button accelerationButton = new Button();
+
+        public void Accelerate()
+        {
+            currentSpeed = 100;
+        }
+        public void IncreaseTextSize()
+        {
+            string result = accelerationButton.MakeTextBigger(250);
+
+            if (result == "fail")
+                accelerationButton.MakeTextBigger(120);
+        }
+    }
+
+    class Button
+    {
+        int fontSize = 60;
+
+        public string MakeTextBigger(int size)
+        {
+
+            if (size <= 200)
+            {
+                fontSize = size;
+                return "Success";
+            }
+            else
+                return "fail";
+            
+        }
+    }
 }
