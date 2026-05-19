6a5274d7a0ee569d61163f6c88cdd9ccfc76fcfc
Oscar J Soriano <ososoriano123@icloud.com>
2026-05-19 10:54:06 -0400

    Created a new message box, check for win, check for draw, reset button, and a status lable.

--- TicTacToe_1/MainWindow.xaml.cs
+++ TicTacToe_1/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -23,13 +24,107 @@ namespace TicTacToe_1
 
         string currentLetter = "X";
 
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            (sender as Button).Content = currentLetter;
-            if (currentLetter == "X")
-                currentLetter = "O";
-            else 
-                currentLetter = "X";
+            var button = sender as TicTacToe_1.Button;
+            if (button.Content != null && button.Content.ToString() != "")
+            {
+                MessageBox.Show("This cell is already occupied. Please choose another one.");
+                return;
+
+                button.Content = currentLetter;
+
+                button.IsEnabled = false;
+
+                if (CheckForWin())
+                {
+                    StatusLable.Content = "Player {currentLetter} wins!";
+
+                    MessageBox.Show("Player {currentLetter} wins!", "Game Over");
+
+                    ResetGame();
+
+                    return;
+                }
+
+                if (CheckForDraw())
+                {
+                    StatusLable.Content = "It's a draw!";
+                    MessageBox.Show("It's a draw!", "Game Over");
+                    ResetGame();
+                    return;
+                }
+
+                currentLetter = currentLetter == "X" ? "O" : "X";
+
+                StatusLable.Content = "Player {currentLetter}'s turn";
+
+            }
+
+           
+            
+        }
+         private bool CheckForWin()
+        {
+            var cells = new string[9];
+            for (int i = 0; i < 8; i++)
+            {
+                var button = GameGrid.Children[i] as TicTacToe_1.Button;
+
+                cells[i] = button.Content?.ToString() ?? "";
+            }
+
+            int[,] winLines = new int[,]
+            {
+                {0, 1, 2}, // Top Row
+                {3, 4, 5}, // Middle Row
+                {6, 7, 8}, // Bottom Row
+                {0, 3, 6}, // Left Column
+                {1, 4, 7}, // Ceter Column
+                {2, 5, 8}, // Right Column
+                {0, 4, 8}, // Diagonal Top-Left to Bottom-Right
+                {2, 4, 6} // Diagonal Top-Right to Bottom-Left
+            };
+
+            for (int i = 0; i < 8; i++)
+            {
+                string a = cells[winLines[i, 0]];
+                string b = cells[winLines[i, 1]];
+                string c = cells[winLines[i, 2]];
+
+                if (a != "" && a == b && b == c)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private bool CheckForDraw()
+        {
+            foreach (TicTacToe_1.Button button in GameGrid.Children)
+            {
+                if (button.Content == null || button.Content.ToString() == "")
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private void ResetGame()
+        {
+            foreach (TicTacToe_1.Button button in GameGrid.Children)
+            {
+                button.Content = "";
+                button.IsEnabled = true;
+            }
+            currentLetter = "X";
+            StatusLable.Content = "Player {currentLetter}'s turn";
         }
     }
 }
--- TicTacToe_1/MyClasses.cs
+++ TicTacToe_1/MyClasses.cs
@@ -3,40 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
 using System.Windows.Media;
 
 namespace TicTacToe_1
 {
-    class Car
-    {
-        String color = "red";
-        protected int currentSpeed = 0;
-        Button accelerationButton = new Button();
-
-        public virtual void Accelerate()
-        {
-            currentSpeed = 100;
-        }
-        public void IncreaseTextSize()
-        {
-            double size = accelerationButton.FontSize; 
-            accelerationButton.FontSize = 1000;
-
-            string result = accelerationButton.MakeTextBigger(250);
-
-            if (result == "fail")
-                accelerationButton.MakeTextBigger(120);
-        }
-    }
-
-    class FastCar : Car
-    {
-        public override void Accelerate()
-        {
-            currentSpeed = 200;
-        }
-    }
-
     class Button : System.Windows.Controls.Button
     {
         public Button()
@@ -47,20 +19,6 @@ namespace TicTacToe_1
             BorderBrush = new SolidColorBrush(Colors.Black);
         }
 
-        public string MakeTextBigger(int size)
-        {
-
-            if (size <= 200)
-            {
-                FontSize = size;
-                return "Success";
-            }
-            else
-                return "fail";
-            
-        }
-
-       
-
     }
+
 }
